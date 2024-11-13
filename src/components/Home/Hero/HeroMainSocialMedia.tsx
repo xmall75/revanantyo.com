94a255b4074db08f1e8c1116d8e1692145a13b11
Revanantyo D <xmall75.id@gmail.com>
2024-11-13 16:52:16 +0700

    feat: add link to social media button

--- src/components/Home/Hero/HeroMainSocialMedia.tsx
+++ src/components/Home/Hero/HeroMainSocialMedia.tsx
@@ -1,32 +1,34 @@
-import { Box, Divider, Grid } from "@chakra-ui/react";
+"use client";
+
+import { Box, Divider, Grid, useColorModeValue } from "@chakra-ui/react";
 
 import { SocialMedia } from "@/constants/social_media";
 
 const HeroMainSocialMedia = () => {
+  const color = useColorModeValue("brownPrimary.700", "blueAccent.500");
+
   return (
     <Box display="flex" justifyContent="center" alignItems="center" gap={6}>
       <Divider borderWidth={2} />
-      {/* <Box
-        width="auto"
-        display="flex"
-        justifyContent="end"
-        alignItems="center"
-        gap={4}
-      >
-        <Box width="auto"></Box>
-      </Box> */}
       <Grid templateColumns="repeat(4, 1fr)" gap={4}>
-        {SocialMedia.map((item, key) => {
+        {SocialMedia.map((social, key) => {
           return (
             <Box
               key={key}
+              as="a"
+              href={social.url}
+              target="_blank"
               display="flex"
               alignItems="center"
               justifyContent="center"
               gap={2}
+              _hover={{
+                color: color,
+              }}
+              transition="color 0.1s ease!important"
             >
-              <item.icon />
-              {item.title}
+              <social.icon />
+              {social.title}
             </Box>
           );
         })}
