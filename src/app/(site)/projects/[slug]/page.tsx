a23a05acc817e40ac6a2a551d2a91c1dc93857c6
Revanantyo D <xmall75.id@gmail.com>
2025-02-15 14:39:48 +0700

    refactor: modify route from project to projects

---
+++ src/app/(site)/projects/[slug]/page.tsx
@@ -0,0 +1,47 @@
+import { Box } from "@chakra-ui/react";
+
+import ProjectDetailContent from "@/components/Project/ProjectDetail/ProjectDetailContent";
+import ProjectDetailHeader from "@/components/Project/ProjectDetail/ProjectDetailHeader";
+
+import { getProjectBySlug } from "@/sanity/utils/project.utils";
+
+const ProjectDetailPage = async ({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}) => {
+  const project = await getProjectBySlug(params.slug);
+
+  return (
+    <Box
+      mt={{
+        base: 36,
+        md: 36,
+        lg: 48,
+      }}
+      width={{
+        base: "92%",
+        sm: "90%",
+        md: "80%",
+        lg: "70%",
+      }}
+      mx="auto"
+    >
+      <ProjectDetailHeader
+        title={project.name}
+        thumbnail={project.images}
+        shortDescription={project.shortDescription}
+        date={project._createdAt}
+      />
+      <ProjectDetailContent
+        content={project.content}
+        github={project.github}
+        url={project.url}
+      />
+    </Box>
+  );
+};
+
+export default ProjectDetailPage;
